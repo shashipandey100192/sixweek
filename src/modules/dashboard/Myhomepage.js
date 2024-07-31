e73a183131f2c9381d82cafe23c9bf8894c74822
shashiweb <shashiweb64@gmail.com>
2024-07-31 17:40:45 +0530

    final corse project

--- src/modules/dashboard/Myhomepage.js
+++ src/modules/dashboard/Myhomepage.js
@@ -2,7 +2,49 @@ import React from 'react'
 
 function Myhomepage() {
   return (
-    <div>Myhomepage</div>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-3'>
+          <div className="card text-bg-success mb-3 shadow">
+            <div className="card-header">Sales</div>
+            <div className="card-body text-center">
+              <h5 className="card-title">Sales Report</h5>
+              <p className="card-text h1">54871 </p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className="card text-bg-primary mb-3 shadow">
+            <div className="card-header">Sales</div>
+            <div className="card-body text-center">
+              <h5 className="card-title">Sales Report</h5>
+              <p className="card-text h1">54871 </p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className="card text-bg-warning mb-3 shadow">
+            <div className="card-header">Sales</div>
+            <div className="card-body text-center">
+              <h5 className="card-title">Sales Report</h5>
+              <p className="card-text h1">54871 </p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3'>
+          <div className="card text-bg-danger mb-3 shadow">
+            <div className="card-header">Sales</div>
+            <div className="card-body text-center">
+              <h5 className="card-title">Sales Report</h5>
+              <p className="card-text h1">54871 </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-6 text-center'>
+        <h1>Place info About your Project </h1>
+      </div>
+    </div>
   )
 }
 
